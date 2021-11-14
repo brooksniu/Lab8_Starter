e7a620f7b50d3ecd25a03e01ae0f308b353d663a
Ruijia Brooks Niu <brooksnew92@gmail.com>
2021-11-13 21:22:39 -0800

    finished expose

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
     Because manually run them locally before pushing code would take time and effort each time the local branch is updated. Also the local environment might be different than everyone else's, so it is easier and safer to run the test through Github actions. If we run all the tests after all development is completed, it would be extremely hard for team to debug and change things since many things are connected in a project and changing some of the componenets might result in a disaster. So run through Github Action is the best option out of three.
 2. No
+
+3. No. Because message feature requires interaction between components(send a message to another user), for which unit tests cannot perform.
+4. Yes. Becuase the "mex message length" feature requires no interaction between components(only checks if the typed characters exceeded 80), for which unit test can handel really well.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,68 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// phone number test
+test("check for valid phone number", () => {
+    expect(functions.isPhoneNumber("888-888-8888")).toBe(true);
+});
+test("check for valid phone number", () => {
+    expect(functions.isPhoneNumber("999-999-9999")).toBe(true);
+});
+test("check for invalid phone number", () => {
+    expect(functions.isPhoneNumber("999999")).toBe(false);
+});
+test("check for invalid phone number", () => {
+    expect(functions.isPhoneNumber("912736487kaiusdg")).toBe(false);
+});
+// email test
+test("check for valid  email", () => {
+    expect(functions.isEmail("powell@gmail.com")).toBe(true);
+});
+test("check for valid  email", () => {
+    expect(functions.isEmail("99sf@whatisthis.com")).toBe(true);
+});
+test("check for invalid  email", () => {
+    expect(functions.isEmail("999-999-9")).toBe(false);
+});
+test("check for invalid  email", () => {
+    expect(functions.isEmail("sadfolijhnvasjd9")).toBe(false);
+});
+// test strong password
+test("check for strong password", () => {
+    expect(functions.isStrongPassword("sfwhatistcom")).toBe(true);
+});
+test("check for strong password", () => {
+    expect(functions.isStrongPassword("professorpowell")).toBe(true);
+});
+test("check for weak password", () => {
+    expect(functions.isStrongPassword("1sssssss")).toBe(false);
+});
+test("check for weak password", () => {
+    expect(functions.isStrongPassword("abc")).toBe(false);
+});
+// test date
+test("check for valid date", () => {
+    expect(functions.isDate("01/12/4987")).toBe(true);
+});
+test("check for valid date", () => {
+    expect(functions.isDate("1/2/1111")).toBe(true);
+});
+test("check for invalid date", () => {
+    expect(functions.isDate("1234/2/1111")).toBe(false);
+});
+test("check for invalid date", () => {
+    expect(functions.isDate("1/2/111")).toBe(false);
+});
+// test hexcolor
+test("check for valid color", () => {
+    expect(functions.isHexColor("c5b9cd")).toBe(true);
+});
+test("check for valid color", () => {
+    expect(functions.isHexColor("cbd")).toBe(true);
+});
+test("check for invalid color", () => {
+    expect(functions.isHexColor("c222b3d")).toBe(false);
+});
+test("check for invalid color", () => {
+    expect(functions.isHexColor("2130984689")).toBe(false);
+});
